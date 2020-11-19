2279da76ff06d54860f800a8123a1662b37c9856
prncn <princen@outlook.de>
2020-11-19 23:31:30 +0100

    Add Visualisation and Update View

--- scripts/graphdos.js
+++ scripts/graphdos.js
@@ -4,17 +4,18 @@ const search_domb = document.getElementById('artist-keyb');
 const result_dom = document.getElementById('results-loader');
 let entry_counter = 0;
 
+
+/**
+ * Load in data from cache on page load.
+ */
 async function load_cache(){
     let response = await fetch('scripts/data_cache.json');
     cache_json = await response.json();
     return cache_json;
 }
 
+
 load_cache().then(resp => {
-    
-    /**
-     * Object describing artist data from LastFM API call.
-     */
     class artistData {
         constructor(obj){
             Object.assign(this, obj)
@@ -39,12 +40,17 @@ load_cache().then(resp => {
     }
 };
 
+
 let DATA_CACHE = resp;
 for(let i=0; i<DATA_CACHE.length; i++){
     DATA_CACHE[i] = new artistData(DATA_CACHE[i])
 }
 
-function is_cached(node){
+/**
+ * Check if cache contains node.
+ * @param {String} node current artistData
+ */
+function is_cached(node) {
     for(let i=0; i<DATA_CACHE.length; i++){
         if(DATA_CACHE[i].origin === node)
             return i;
@@ -60,13 +66,13 @@ function is_cached(node){
  */
 async function fetch_similars(artist) {
     const api_key = '74276898ee7faad0825b302a0abe5f07' 
-    const main_url = 'https://ws.audioscrobbler.com/2.0/?'
+    const endpoint = 'https://ws.audioscrobbler.com/2.0/?'
     const limit = 15;
     const autocorrect = 1;
-    artist_data = new artistData;
+    let artist_data = new artistData();
     
     try {
-        let data = await fetch(`${main_url}method=artist.getsimilar&limit=${limit}&artist=${artist}&api_key=${api_key}&autocorrect=${autocorrect}&format=json`)
+        let data = await fetch(`${endpoint}method=artist.getsimilar&limit=${limit}&artist=${artist}&api_key=${api_key}&autocorrect=${autocorrect}&format=json`)
         data = await data.json();
         artist_data.origin = await data.similarartists["@attr"].artist;
         let similars_data = await data.similarartists.artist.filter(x => !x.name.includes("&"));
@@ -86,8 +92,13 @@ async function fetch_similars(artist) {
  * Execute path functions and render results to HTML.
  */
 async function render_artists() {
+    pos.clear();
     let artistkey_a = search_doma.value;
     let artistkey_b = search_domb.value;
+    if(artistkey_a === '' || artistkey_b === '')
+    return 
+    document.getElementById('search-btn').classList.remove('mx-sm-2');
+    document.getElementById("graph-container").innerHTML = '';
     found_dom.innerHTML = '';
     result_dom.classList.add('logo-bm');
     bodymovin.loadAnimation({
@@ -115,29 +126,107 @@ async function render_artists() {
         found_dom.insertAdjacentHTML('beforeend', `<li>${name}</li>`);
     }
     found_dom.insertAdjacentHTML('beforeend', `<br><strong>Match Rate: ${(match_rate*100).toFixed(1)}%</strong>`);
-    found_dom.insertAdjacentHTML('beforeend', `<br>${((t1 - t0)/1000).toFixed(2)} seconds. ${entry_counter} entries`);
-
-    let cache_string = [];
-    for(let item of DATA_CACHE){
-        // console.log(item)
-        // console.log(JSON.stringify(item));
-        cache_string.push(JSON.stringify(item));
-    }
-    // console.log(DATA_CACHE, cache_string)
-    // let a = document.createElement('a');
-    // a.href = "data:application/octet-stream,"+encodeURIComponent(cache_string);
-    // a.download = 'abc.txt';
-    // a.click();
-    console.log(DATA_CACHE);
+    found_dom.insertAdjacentHTML('beforeend', `<p style="color: var(--prime-color)">${((t1 - t0)/1000).toFixed(2)} seconds. ${entry_counter} entries</p>`);
+    // document.getElementById("graph-container").innerHTML = '';
+    render_markpath(seperation_path);
+    document.getElementById('search-btn').classList.add('mx-sm-2');
 }
 document.getElementById('search-btn').onclick = render_artists;
 
 
 /**
+ * Used to download current DATA_CACHE to prepare for JSON file. 
+ * Not for deployment.
+ */
+function download_cache(){
+    let cache_string = [];
+    for(let item of DATA_CACHE){
+        cache_string.push(JSON.stringify(item));
+    }
+
+    let a = document.createElement('a');
+    a.href = "data:application/octet-stream,"+encodeURIComponent(cache_string);
+    a.download = 'data_cache.json';
+    a.click();
+}
+
+
+/**
+ * Render artistData name to random position on HTML body.
+ * @param {String} node 
+ */
+const graph_container = document.getElementById("graph-container");
+var fullWidth = graph_container.clientWidth;
+var fullHeight = graph_container.clientHeight; 
+// let xpos = (fullWidth / 2);
+// let ypos = (fullHeight / 2);
+let xpos = 500;
+let ypos = 500;
+let pos = new Set();
+function render_randomnode(node){
+    var maxwidth = window.innerWidth;
+    var maxheight = window.innerHeight;
+
+    const elem = document.createElement("div");
+    elem.classList.add('rendered-node');
+    elem.innerText = node;
+    var rsgn = () => Math.round(Math.random()) ? 1 : -1;
+    xpos = Math.round(rsgn() * (Math.round(Math.random() * fullWidth)* 0.25 + xpos * 0.75)); 
+    ypos = Math.round(rsgn() * (Math.round(Math.random() * fullHeight)* 0.25 + ypos * 0.75));
+
+    if(pos.has(xpos)){
+        // console.log("detected");
+        while(pos.has(xpos)){
+            xpos = xpos + 50;
+            ypos = rsgn() * (Math.round(Math.random() * fullHeight)* 0.25 + ypos * 0.75);
+        }
+    } else {
+        // console.log("not detected");
+        pos.add(xpos);
+    }
+    elem.style.left = xpos + "px";
+    elem.style.top = ypos + "px";
+
+    // fullWidth += 10;
+    // fullHeight += 10;
+    graph_container.appendChild(elem);
+    // graph_container.style.width = fullWidth + "px";
+    // graph_container.style.height = fullHeight + "px";
+}
+
+
+/**
+ * Highlight seperation path in render.
+ * @param {Array of artistData} seperation_path path from source to destination
+ */
+function render_markpath(seperation_path){
+    let nodes = document.getElementsByClassName('rendered-node');
+    let parentx = 0;
+    let parenty = 0;
+    for(let j=0; j<seperation_path.length; j++){
+        for(let i=0; i<nodes.length; i++){
+            if(nodes[i].innerText === seperation_path[j]){
+                nodes[i].style.zIndex = 5;
+                nodes[i].style.color = "red";
+                // nodes[i].style.backgroundColor = "white";
+                if(j > 0)
+                graph_container.insertAdjacentHTML('beforeend', 
+                `<svg style="position: absolute;" width="${window.innerWidth}"height="${window.innerHeight}">
+                <line x1="${nodes[i].style.left}" y1="${nodes[i].style.top}" x2="${parentx}" y2="${parenty}" stroke="red"/>
+                </svg>`)
+                parentx = nodes[i].style.left;
+                parenty = nodes[i].style.top;
+            }
+        }
+    }
+}
+
+
+/**
  * Checks if there is an intersection between neighbor nodes of node A neighbors of B.
  * Bi-directional BFS graph search.
- * @param {string} artist_a     first artist
- * @param {string} artist_b     second artist
+ * @param {String} artist_a     first artist
+ * @param {String} artist_b     second artist
  */
 async function bidirect_search(artist_a, artist_b) {
     entry_counter = 0;
@@ -180,12 +269,13 @@ async function bidirect_search(artist_a, artist_b) {
     }
 }
 
+
 /**
  * Increase size of BFS search zone.
- * @param {array of strings} nodes      list of total BFS area 
- * @param {array of nums} preds         list of predecessors
- * @param {set of strings} visited      nodes that have already been discovered
- * @param {array of nums} match_rates   match rates corresponding to nodes list
+ * @param {Array of String} nodes      list of total BFS area 
+ * @param {Array of Int} preds         list of predecessors
+ * @param {Set of String} visited      nodes that have already been discovered
+ * @param {Array of Float} match_rates   match rates corresponding to nodes list
  */
 async function update_bfs(nodes, preds, visited, matches){
     for (let node of nodes){
@@ -194,6 +284,7 @@ async function update_bfs(nodes, preds, visited, matches){
         // console.log(node);
         if(visited.has(node))
             continue;
+        render_randomnode(node);
 
         let cached = is_cached(node); 
         let data;
@@ -216,9 +307,9 @@ async function update_bfs(nodes, preds, visited, matches){
 
 /**
  * Returns the parent node of a given child node.
- * @param {array of strings} nodes  list of total BFS area 
- * @param {array of strings} preds  list of predecessors
- * @param {string} child            child node of which the parent will be searched
+ * @param {Array of String} nodes  list of total BFS area 
+ * @param {Array of String} preds  list of predecessors
+ * @param {String} child            child node of which the parent will be searched
  */
 function get_parent(nodes, preds, child) {
     let child_index = nodes.indexOf(child);
@@ -238,13 +329,13 @@ function get_parent(nodes, preds, child) {
 
 /**
  * Traces back path from destination to intersection node, from intersection to source node, then concatenates.
- * @param {array of strings} nodes_a    list of total BFS area of source a
- * @param {array of nums} preds_a       list of predecessors of source a
- * @param {array of strings} nodes_b    list of total BFS area of destination b
- * @param {array of nums} preds_b       list of predecessors of destination b
- * @param {string} inters               intersecting node 
- * @param {string} artist_a             source node
- * @param {string} artist_b             destination node
+ * @param {Array of String} nodes_a    list of total BFS area of source a
+ * @param {Array of Int} preds_a       list of predecessors of source a
+ * @param {Array of String} nodes_b    list of total BFS area of destination b
+ * @param {Array of Int} preds_b       list of predecessors of destination b
+ * @param {String} inters               intersecting node 
+ * @param {String} artist_a             source node
+ * @param {String} artist_b             destination node
  */
 function trace_path(nodes_a, preds_a, nodes_b, preds_b, inters, artist_a, artist_b) {
     let path = [];
@@ -275,11 +366,11 @@ function trace_path(nodes_a, preds_a, nodes_b, preds_b, inters, artist_a, artist
 
 /**
  * Calculate smallest match rate based on list of all match rates.
- * @param {array of artistData} path    path from source to destination
- * @param {array of strings} nodes      list of predecessors
- * @param {array of nums} matches       match rates corresponding to nodes list
+ * @param {Array of artistData} path    path from source to destination
+ * @param {Array of String} nodes      list of predecessors
+ * @param {Array of Int} matches       match rates corresponding to nodes list
  */
-function get_matchrate(path, nodes, matches){
+function get_matchrate(path, nodes, matches) {
     let match_list = [];
     for(let item of path){
         let child_index = nodes.indexOf(item);
